3dc545a224aa044c7d174f53189cc019aeaa56e0
Rydog101 <ryanjeastland@gmail.com>
2015-03-07 09:13:50 +1100

    cleaned up

--- src/main/java/net/rystuff/mcjsonsound/Main.java
+++ src/main/java/net/rystuff/mcjsonsound/Main.java
@@ -19,19 +19,7 @@ public class Main
             {
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             }
-            catch (ClassNotFoundException e)
-            {
-                e.printStackTrace();
-            }
-            catch (InstantiationException e)
-            {
-                e.printStackTrace();
-            }
-            catch (IllegalAccessException e)
-            {
-                e.printStackTrace();
-            }
-            catch (UnsupportedLookAndFeelException e)
+            catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e)
             {
                 e.printStackTrace();
             }
